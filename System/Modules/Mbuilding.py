0c51656d174217a1a5299a9f6349365fb4067a08
TomasNFH <tomasnfh1@gmail.com>
2025-05-17 21:17:16 -0300

    FS loop 6

--- System/Modules/Mbuilding.py
+++ System/Modules/Mbuilding.py
@@ -171,7 +171,7 @@ def model_shake(DATA, TARGET_COLUMN, TARGET_TY, Fast = True):
             best_set_score = feat_n_score.sort_values(by='Score').iloc[-1]['Score']
             best_set_importances = feat_n_score.sort_values(by='Score').iloc[-1]['importances']
 
-            Feat_best_set.loc[len(Feat_best_set.index)] = [model_nm, feature_nm, best_set, best_set_score] 
+            Feat_best_set.loc[len(Feat_best_set.index)] = [model_nm, feature_nm, best_set, best_set_importances, best_set_score] 
             
 
 
