458cdcc61ecfecd3fbd847f55c5ef9f885a77f2d
Tomás Ferraz <tomasnfh1@gmail.com>
2025-06-02 23:05:23 -0300

    Update Fselection.py

--- System/Modules/Fselection.py
+++ System/Modules/Fselection.py
@@ -1,60 +1,75 @@
 from sklearn.ensemble import RandomForestRegressor
 from mlxtend.feature_selection import ExhaustiveFeatureSelector as EFS
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
 
 
 def F_selector(X_train, y_train, N_features=5, FLAG=0):
-    '''
-    input: X_train, y_train - used for training
-    return:
-    '''
+    """
+    Feature selection function.
+
+    Parameters:
+        X_train (pd.DataFrame): Training features.
+        y_train (pd.Series or pd.DataFrame): Training labels.
+        N_features (int): Number of top features to select.
+        FLAG (int): Selection method:
+            0 - Random Forest feature importance
+            1 - Correlation-based selection
+            2 - Exhaustive Feature Selector (EFS) with Logistic Regression
+
+    Returns:
+        X_reduced (np.ndarray): Reduced feature set.
+        selected_features (pd.Index): Names of selected features.
+        importances_selected (np.ndarray): Importance values of selected features.
+        selected_indexes (np.ndarray): Indexes of selected features.
+    """
     if FLAG == 2: 
-        if X.shape[1]>N_features: Mf = N_features
-        else: Mf = X.shape[1]
+        # Limit maximum number of features to those present in the dataset
+        Mf = min(X_train.shape[1], N_features)
 
         #Create an EFS object
-        efs = EFS(estimator=LogisticRegression(),        # Use logistic regression as the classifier/estimator
-                min_features=1,      # The minimum number of features to consider is 1
-                max_features=Mf,      # The maximum number of features to consider is 4
-                scoring='accuracy',  # The metric to use to evaluate the classifier is accuracy 
-                cv=5,print_progress=True)
-        # breakpoint()
-        efs = efs.fit(X_train, y_train)
-        breakpoint()
-        # efs.finalize_fit()
-
-        features = pd.Index(efs.best_feature_names_)
-        X_reduced = X[features] 
+        efs = EFS(estimator=LogisticRegression(),       
+                min_features=1,      
+                max_features=Mf,     
+                scoring='accuracy',  
+                cv=5, print_progress=True)
+        efs = efs.fit(X_train.to_numpy(), y_train.to_numpy().ravel())
+        
+        # Get selected feature indexes from their names
+        indexes = list(map(int, efs.best_feature_names_))
+        features = X_train.columns[indexes]
+        X_reduced = X_train.iloc[:, indexes]
         importancesRET = np.ones(len(features))*efs.best_score_ 
 
     else:
         if FLAG == 0:
+            # Feature importance via Random Forest
             model = RandomForestRegressor()
             model.fit(X_train.to_numpy(), y_train.to_numpy().ravel())
-            importances = model.feature_importances_ #puedo cambiar el impurity de Gini a otro, probar
+            importances = model.feature_importances_ # I can change the Gini impurity to another criterion, worth testing
         if FLAG == 1:
-            # breakpoint()
+            # Feature importance via correlation with target
             data = pd.concat([y_train, X_train], axis=1)
             corrM = data.corr() 
             importances = corrM.iloc[0][1:]
             importances = importances.to_numpy()
             importances = np.abs(importances)
             importances = importances[np.logical_not(np.isnan(importances))] 
+            
+        # Sort importances and select the top N    
         imp_sorted = np.sort(importances)
         if len(importances) <= N_features: importancesRET = imp_sorted
         else: importancesRET = imp_sorted[len(importances)-N_features:]
+        
+        # Retrieve indexes corresponding to selected importance values
         indexes = np.ones(0, dtype = int)
-        #we run over the N_Features more importante and finde the index of each one
         for importance in importancesRET: 
             idx = np.where(importances == importance)[0][0]
             indexes = np.append(indexes, int(idx))
-        features = X_train.columns[indexes] #select the column names by the indexes
-        X_reduced = X_train.iloc[:, indexes] #reduce X by the most important feature
-        X_reduced = X_reduced.to_numpy() #input was DF, i need np array to model.fit()
+
+        features = X_train.columns[indexes] 
+        X_reduced = X_train.iloc[:, indexes] 
+        X_reduced = X_reduced.to_numpy()
     return X_reduced, features , importancesRET, indexes
 
