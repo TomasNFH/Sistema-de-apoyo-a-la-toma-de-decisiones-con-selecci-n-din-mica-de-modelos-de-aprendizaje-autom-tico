9e5043099908b2cb894499498744912aee6c0574
Tomás Ferraz <tomasnfh1@gmail.com>
2025-06-02 23:02:41 -0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,4 +1,9 @@
 from System import Workflow
+import warnings
+import cutie
 
-ret = Workflow.dyn_model_selection(FAST = True, PLOT = True, local_file=False)
+Fast = cutie.prompt_yes_or_no("Fast or Slow mode?")
+if not Fast: warnings.filterwarnings("ignore")
 
+ret = Workflow.dyn_model_selection(FAST = Fast, PLOT = True, local_file=False)
+breakpoint()
